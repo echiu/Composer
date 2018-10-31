3e8138f8ae0e81115ca8aa97865af5664a816aba
Eric Chiu <echiu1997@gmail.com>
2018-10-31 03:14:30 -0400

    corrected piano modes

--- src/main.js
+++ src/main.js
@@ -36,13 +36,16 @@ var dorian =     [2, 1, 2, 2, 2, 1, 2];
 var phygian =    [1, 2, 2, 2, 1, 2, 2];
 var lydian =     [2, 2, 2, 1, 2, 2, 1];
 var mixolydian = [2, 2, 1, 2, 2, 1, 2];
-var aeolian =    [2, 1, 2, 2, 2, 1, 2];
-var locrian =    [2, 1, 2, 1, 2, 2, 2];
+var aeolian =    [2, 1, 2, 2, 1, 2, 2];
+var locrian =    [1, 2, 2, 1, 2, 2, 2];
 var modes = [ionian, dorian, phygian, lydian, mixolydian, aeolian, locrian];
 
 // probabilities of chord progressions
 // progressions[0][0] means there is a 0.10 chance to get from I to I
 // progressions[4][0] means there is a 0.95 chance ot get from V to I
+// see diagrams here:
+// http://www.angelfire.com/music/HarpOn/image/chordprogmaj.gif
+// http://www.angelfire.com/music/HarpOn/image/chordprogmin.gif
 var progressions = [
   [0.10, 0.05, 0.05, 0.50, 0.20, 0.05, 0.05],
   [0.00, 0.00, 0.00, 0.00, 0.90, 0.00, 0.10],
@@ -52,9 +55,24 @@ var progressions = [
   [0.00, 0.75, 0.00, 0.20, 0.05, 0.00, 0.00],
   [0.70, 0.00, 0.00, 0.00, 0.30, 0.00, 0.00]];
 
+var startNote = [];
+
+function getNextProgression(oldNote)
+{
+  var seed = Math.random();
+  var sumProbability = 0.0;
+  // iterate through the probabilities of the oldNote row
+  for (var i = 0; i < progressions[oldNote].length; i++) 
+  {
+    sumProbability += progressions[oldNote][i];
+    if (seed <= sumProbability) { return i; }
+  }
+  return undefined;
+}
+
 // generates notes for one KEY, for a random number of measures
 // then passes it on to another KEY
-function generateNotes()
+function generateMelody()
 {
   // step one is to randomly choose the mode
   var random = Math.floor(Math.random() * modes.length);
@@ -62,27 +80,41 @@ function generateNotes()
 
   // step two is to play chords in mode
   random = Math.floor(Math.random() * 2);
-  if (random == 0 && index + mode[0] + mode[1] + mode[2] + mode[3] < length)
+  var isArpeggio = Math.floor(Math.random() * 2);
+  if (random == 0 && index + currMode[0] + currMode[1] + currMode[2] + currMode[3] < length)
   {
     notesQueue.push(index);
-    notesQueue.push(index + mode[0] + mode[1]);
-    notesQueue.push(index + mode[0] + mode[1] + mode[2] + mode[3]);
-    countQueue.push(3);
-    //countQueue.push(1);
-    //countQueue.push(1);
-    //countQueue.push(1);
-    index = index + mode[0] + mode[1] + mode[2] + mode[3];
+    notesQueue.push(index + currMode[0] + currMode[1]);
+    notesQueue.push(index + currMode[0] + currMode[1] + currMode[2] + currMode[3]);
+    if (isArpeggio)
+    {
+      countQueue.push(1);
+      countQueue.push(1);
+      countQueue.push(1);
+    }
+    else
+    {
+      countQueue.push(3);
+    }
+
+    index = index + currMode[0] + currMode[1] + currMode[2] + currMode[3];
   }
-  else if (random == 1 && index - mode[3] - mode[2] - mode[1] - mode[0] >= 0)
+  else if (random == 1 && index - currMode[3] - currMode[2] - currMode[1] - currMode[0] >= 0)
   {
     notesQueue.push(index);
-    notesQueue.push(index - mode[3] - mode[2]);
-    notesQueue.push(index - mode[3] - mode[2] - mode[1] - mode[0]);
-    countQueue.push(3);
-    //countQueue.push(1);
-    //countQueue.push(1);
-    //countQueue.push(1);
-    index = index - mode[3] - mode[2] - mode[1] - mode[0];
+    notesQueue.push(index - currMode[3] - currMode[2]);
+    notesQueue.push(index - currMode[3] - currMode[2] - currMode[1] - currMode[0]);
+    if (isArpeggio)
+    {
+      countQueue.push(1);
+      countQueue.push(1);
+      countQueue.push(1);
+    }
+    else
+    {
+      countQueue.push(3);
+    }
+    index = index - currMode[3] - currMode[2] - currMode[1] - currMode[0];
   }
 }
 
@@ -95,6 +127,20 @@ function onLoad(framework)
   var controls = framework.controls;
   //var gui = framework.gui;
   //var stats = framework.stats;
+
+  // check if all rows in progressions add up to 1.0
+  for (var i = 0; i < progressions.length; i++)
+  {
+    var probabilitySum = 0.0;
+    for (var j = 0; j < progressions[i].length; j++)
+    {
+      probabilitySum += progressions[i][j];
+    }
+    if (probabilitySum != 1.0)
+    {
+      console.log("ERROR: progression probability for row " + i + " does not add up to 1.0");
+    }
+  }
 
   // load all the piano note mp3 files
   for (var i = 0; i < length; i++)
@@ -143,7 +189,7 @@ function onUpdate(framework)
 
     if (notesQueue.length < 100)
     {
-      generateNotes();
+      generateMelody();
     }
 
     startTime = Date.now();
